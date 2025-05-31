5921109d3f075f4e00f45362a773f4e0e67848dd
Paulo <pvvtoledo@gmail.com>
2025-05-31 07:51:40 -0300

    SSG e formatação do preço

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import "keen-slider/keen-slider.min.css";
 
 
 import { stripe } from "@/lib/stripe";
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Stripe from "stripe";
 
 interface HomeProps {
@@ -42,7 +42,7 @@ export default function Home({ products }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ["data.default_price"],
   });
@@ -58,14 +58,18 @@ export const getServerSideProps: GetServerSideProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount / 100,
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      }).format(price.unit_amount / 100),
     };
   });
 
   return {
     props: {
       products
-    }
+    },
+    revalidate: 60*60*2 //a cada 2h
   }
 
 };
